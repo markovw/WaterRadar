274f2109aca6988a076b5b2e65035f2bddc9a2cc
markovw <randyhelper@gmail.com>
2024-05-11 00:43:59 +0300

    Initial Commit

---
+++ hydrated-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hydrated-app
+//
+//  Created by Vladislav on 11.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hydrated-app/hydrated_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydrated_appApp.swift
+//  hydrated-app
+//
+//  Created by Vladislav on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct hydrated_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
