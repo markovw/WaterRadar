70ff052e4d43cdbccb379d7645ce027e4b029db4
markovw <randyhelper@gmail.com>
2024-05-11 13:40:31 +0300

    Added water adding logic

--- hydrated-app/WaterView.swift
+++ hydrated-app/WaterView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 struct WaterView: View {
     var body: some View {
         
+        VStack {
+            Spacer()
+            
+            DropShape()
+                .stroke(Color.blue, lineWidth: 6)
+                .frame(width: 300, height: 400)
+//            Image(systemName: "drop")
+//                .resizable()
+//                .frame(width: 200, height: 300)
+//                .foregroundStyle(.cyan)
+            Spacer()
+            ButtonAF(action: {
+                
+            }, buttonText: "Add water", icon: "")
+        }
+    }
+}
+
+struct DropShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
         
-        Image(systemName: "drop")
-            .resizable()
-            .frame(width: 200, height: 300)
-            .foregroundStyle(.cyan)
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addCurve(to: CGPoint(x: rect.midX, y: rect.maxY - rect.height * 0.1),
+                      control1: CGPoint(x: rect.maxX, y: rect.minY + rect.height * 0.4),
+                      control2: CGPoint(x: rect.maxX, y: rect.maxY - rect.height * 0.1))
+        path.addCurve(to: CGPoint(x: rect.midX, y: rect.minY),
+                      control1: CGPoint(x: rect.minX, y: rect.maxY - rect.height * 0.1),
+                      control2: CGPoint(x: rect.minX, y: rect.minY + rect.height * 0.4))
+        path.closeSubpath()
         
+        return path
     }
 }
 
